325e1952c700129b2337ad5675f11fa87f7f56d7
Lenz Gschwendtner <lenz@springtimesoft.com>
2011-05-27 17:34:25 +1200

    Checking in changes prior to tagging of version 0.8.
    Changelog diff is:
    
    diff --git c/Changes w/Changes
    index 3f9b4ae..3853483 100644
    --- c/Changes
    +++ w/Changes
    @@ -1,5 +1,8 @@
     Revision history for Daemonise
    
    +0.8     2011-05-27T17:34:12
    +        fixed a too early channel_close
    +
     0.7     2011-05-27T14:01:26
             added a msg_rpc command for rpc calls

--- lib/Daemonise.pm
+++ lib/Daemonise.pm
@@ -5,7 +5,7 @@ use POSIX qw(strftime SIGINT SIG_BLOCK SIG_UNBLOCK);
 use Config::Any;
 use Unix::Syslog;
 
-our $VERSION = '0.7';
+our $VERSION = '0.8';
 
 has 'user' => (
     is      => 'rw',
@@ -471,7 +471,7 @@ Daemonise - a general daemoniser for anything...
 
 =head1 VERSION
 
-Version 0.7.6.6.5.5.5.5.5.5.4
+Version 0.8.7.6.6.5.5.5.5.5.5.4
 
 =head1 SYNOPSIS
 
