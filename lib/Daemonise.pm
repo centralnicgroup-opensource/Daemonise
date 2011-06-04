5f9ee337d606e121833dc9efae0bb6ef571d45de
Tobias Kirschstein <lev@ideegeo.com>
2011-06-04 20:33:01 +0200

    Checking in changes prior to tagging of version 0.15.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index cef8f7d..727fc69 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Daemonise
    
    +0.15    2011-06-04T20:32:55
    +        re-enabled STDOUT redirection to logfile
    +
     0.14    2011-06-02T20:36:34
             re-added Module::Install bits

--- lib/Daemonise.pm
+++ lib/Daemonise.pm
@@ -5,7 +5,7 @@ use POSIX qw(strftime SIGINT SIG_BLOCK SIG_UNBLOCK);
 use Config::Any;
 use Unix::Syslog;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 has 'user' => (
     is      => 'rw',
@@ -472,7 +472,7 @@ Daemonise - a general daemoniser for anything...
 
 =head1 VERSION
 
-Version 0.14
+Version 0.15.14
 
 =head1 SYNOPSIS
 
