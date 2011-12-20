9759511ab722638f7d9392b2996b4b254f0a9285
Tobias Kirschstein <lev@ideegeo.com>
2011-12-20 18:26:20 +1300

    Checking in changes prior to tagging of version 1.7.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 68c7808..706eb9f 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Daemonise
    
    +1.7     2011-12-20T18:25:25
    +        chomp $msg before sending it to syslog
    +
     1.6     2011-12-03T18:57:25
             use Tie::Syslog to bind STDOUT/STDERR instead

--- lib/Daemonise.pm
+++ lib/Daemonise.pm
@@ -456,7 +456,7 @@ Daemonise - a general daemoniser for anything...
 
 =head1 VERSION
 
-Version 1.6.5.4.4.3.2.2.1.4.1.3.1.2.1.2.1.1.0.6.0.5.0.5.0.5
+Version 1.7.6.5.4.4.3.2.2.1.4.1.3.1.2.1.2.1.1.0.6.0.5.0.5.0.5
 
 =head1 SYNOPSIS
 
