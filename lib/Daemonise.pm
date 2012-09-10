e2f52a893f7774482c5bd5e111e48730149c85b0
Tobias Kirschstein <lev@ideegeo.com>
2012-09-10 18:56:44 +0400

    new plugin HipChat providing a notify() method

--- lib/Daemonise.pm
+++ lib/Daemonise.pm
@@ -5,7 +5,7 @@ use POSIX qw(strftime SIGINT SIG_BLOCK SIG_UNBLOCK);
 use Config::Any;
 use Unix::Syslog;
 
-our $VERSION = '1.22';
+our $VERSION = '1.23';
 
 has 'user' => (
     is      => 'rw',
@@ -472,7 +472,7 @@ Daemonise - a general daemoniser for anything...
 
 =head1 VERSION
 
-Version 1.22.22.22
+Version 1.23
 
 =head1 SYNOPSIS
 
